5bce527fb7a7b8830e3cd24570db36453defdab8
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-06 14:07:55 -0500

    I was able to fix the columns for in the javascript to make sure each col had a use.

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,16 @@ var saveBtn = $(".saveBtn");
 
 var agenda = [];
 
+//init ()
+function init () {
+    var storedAgenda = JSON.parse(localStorage.getItem("agenda"));
+    if (storedAgenda !== null){
+        agenda = storedAgenda;
+    }
+//renderAgenda ();
+}
+
+
 
 //$("#currentDay").text(DateTime.local());
 $("#currentDay").text(luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' }));
