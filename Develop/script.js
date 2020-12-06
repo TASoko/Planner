efac179d30d85ae842bd8b33234e9b1ba32709a0
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-06 16:35:25 -0500

    If/Else statements were added to the javascript in order to change the background of the rows according to the time. This required formating time and adding parseInt. This was done with the help of a TA.

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,49 @@
-var hours = $(".hour");
-var timeBlock = $(".time-block");
-var saveBtn = $(".saveBtn");
+// var hours = $(".hour");
+// var timeBlock = $(".time-block");
+// var saveBtn = $(".saveBtn");
 
 var agenda = [];
 
 //init ()
-function init () {
-    var storedAgenda = JSON.parse(localStorage.getItem("agenda"));
-    if (storedAgenda !== null){
-        agenda = storedAgenda;
-    }
-//renderAgenda ();
+function init() {
+  var storedAgenda = JSON.parse(localStorage.getItem("agenda"));
+  if (storedAgenda !== null) {
+    agenda = storedAgenda;
+  }
+  //renderAgenda ();
 }
 
-
-
 //$("#currentDay").text(DateTime.local());
-$("#currentDay").text(luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' }));
+$("#currentDay").text(
+  luxon.DateTime.local().toLocaleString({
+    weekday: "long",
+    month: "long",
+    day: "2-digit",
+  })
+);
+
+function timeChange() {
+
+let currentTime = luxon.DateTime.local().toLocaleString({ hour: "2-digit", hour12: false });
+//console.log(currentTime);
+let formattedHour = parseInt(currentTime[0] + currentTime[1]);
+
+$(".time-block").each(function (){
+    //console.log(this);
+    let currentHour = parseInt($(this).attr("id"));
+
+  if (formattedHour === currentHour) {
+    $(this).addClass("present");
+  }
+  else if (formattedHour < currentHour) {
+      $(this).addClass("future");
+  }
+    else (formattedHour > currentHour) 
+    {
+        $(this).addClass("past");
+    }
+})
+
+//console.log(currentTime);
+}
+timeChange();
