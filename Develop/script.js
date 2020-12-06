1f4b75773119e946d076c8e2b2c137641e292edf
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-06 18:30:34 -0500

    I've been able to add a function to the js so that when the page is refreshed the text stays.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,6 @@
 // var hours = $(".hour");
 // var timeBlock = $(".time-block");
 
-
 var agenda = [];
 
 //init ()
@@ -23,39 +22,49 @@ $("#currentDay").text(
 );
 
 function timeChange() {
+  let currentTime = luxon.DateTime.local().toLocaleString({
+    hour: "2-digit",
+    hour12: false,
+  });
+  //console.log(currentTime);
+  let formattedHour = parseInt(currentTime[0] + currentTime[1]);
 
-let currentTime = luxon.DateTime.local().toLocaleString({ hour: "2-digit", hour12: false });
-//console.log(currentTime);
-let formattedHour = parseInt(currentTime[0] + currentTime[1]);
-
-$(".time-block").each(function (){
+  $(".time-block").each(function () {
     //console.log(this);
     let currentHour = parseInt($(this).attr("id"));
 
-  if (formattedHour === currentHour) {
-    $(this).addClass("present");
-  }
-  else if (formattedHour < currentHour) {
+    if (formattedHour === currentHour) {
+      $(this).addClass("present");
+    } else if (formattedHour < currentHour) {
       $(this).addClass("future");
-  }
-    else (formattedHour > currentHour) 
+    } else formattedHour > currentHour;
     {
-        $(this).addClass("past");
+      $(this).addClass("past");
     }
-})
+  });
 
-//console.log(currentTime);
+  //console.log(currentTime);
 }
 
 var saveBtn = $(".saveBtn");
 
-saveBtn.on("click", function() {
-    let hour = $(this).siblings(".hour").text();
-    let input = $(this).siblings("#input").val();
-    // console.log(input);
-    // console.log(hour);
+saveBtn.on("click", function () {
+  let hour = $(this).siblings(".hour").text();
+  let input = $(this).siblings("#input").val();
+  // console.log(input);
+  // console.log(hour);
 
-    localStorage.setItem(hour, input);
-})
+  localStorage.setItem(hour, input);
+});
 
+function Storing (){
+    $(".hour").each(function(){
+        let time = $(this).text();
+        let text = localStorage.getItem(time);
+    if (text !== null) {
+        $(this).siblings("textarea").val(text);
+    }
+    });
+}
 timeChange();
+Storing();
